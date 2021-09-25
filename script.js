37eec522945105318f13a0ea1959502c8606315c
Idahosa Ogieva <ida@Idahosas-MacBook-Air.local>
2021-09-24 21:07:21 -0400

    change keyboard to two rows

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*----- constants -----*/
-const country = ["algeria", "seychelles", "ghana"];
-const singer = ["dido", "pharell", "eagles"];
-const city = ["london", "miami", "zihuatenejo"];
+const countries = ["algeria", "seychelles", "ghana"];
+const singers = ["dido", "pharell", "eagles"];
+const cities = ["london", "miami", "zihuatenejo"];
 
 /*----- app's state (variables) -----*/
 let spot;
@@ -18,7 +18,10 @@ let citiesBtn = document.getElementById("btn1");
 let countriesBtn = document.getElementById("btn2");
 let singersBtn = document.getElementById("btn3");
 let categories = document.getElementsByClassName("cat-selectorBtn");
+let categoriesEl = document.getElementsByClassName("select-cat")[0]   //get elementsbyclassname returns a nodelist. so if there's only one element, you need [0]
+
 let header = document.querySelector(".header");
+let letters = document.createElement("div")
 let lettersFirstRow = document.createElement("div");
 let lettersSecondRow = document.createElement("div");
 let lettersOneRow = document.createElement("div");
@@ -30,23 +33,65 @@ for (let category of categories){
     category.addEventListener("click", init, true);
 }
 
+letters.addEventListener("click", displayGuesses)
+
 /*----- functions -----*/
+
+function displayGuesses(e){
+    console.log(e)
+    console.log(e.target.id)
+    let guess = document.getElementById(e.target.id)
+    guess.style.backgroundColor = "white";
+    console.log(guess.style.backgroundColor)
+}
+
 function init(e){
     header.textContent = e.target.textContent;
     console.log(e);
+    letters.id = "letters";
     lettersFirstRow.classList.add("first-row");
-    container.appendChild(lettersFirstRow);
+    letters.appendChild(lettersFirstRow);
+    // container.appendChild(letters);
 
     lettersSecondRow.classList.add("second-row");
-    container.appendChild(lettersSecondRow);
+    letters.appendChild(lettersSecondRow);
+    // container.appendChild(letters);
 
     lettersOneRow.classList.add("one-row");
-    container.appendChild(lettersOneRow);
+    letters.appendChild(lettersOneRow);
+    container.appendChild(letters);
+    
     
     alphabetsArray.forEach(keyboard);
+    
+    categoriesEl.remove();
+    
+    const categoryId = String(e.target.id);
+    if (categoryId === "cities"){
+        array = cities;
+        console.log(array)
+    } else if (categoryId === "countries"){
+        array = countries;
+        console.log(array)
+    } else if (categoryId === "singers"){
+        array = singers;
+        console.log(array)
+    }
 }
 
 function keyboard(letter){
+    
+    
+    // let button2 = document.createElement("div");
+    // button2.textContent = letter;
+    // button2.id = letter.toLowerCase();
+    // button2.style.border = "2px solid white";
+    // button2.style.color = "white";
+    // button2.style.backgroundColor = "pink"
+    // button2.style.textAlign = "center"
+    // button2.classList.add("one-row");
+    // lettersOneRow.appendChild(button2);
+
     let button1 = document.createElement("div");
     button1.textContent = letter;
     button1.id = letter.toLowerCase();
@@ -61,16 +106,7 @@ function keyboard(letter){
         button1.classList.add("second-row");
         lettersSecondRow.appendChild(button1);
     }
-    
-    let button2 = document.createElement("div");
-    button2.textContent = letter;
-    button2.id = letter.toLowerCase();
-    button2.style.border = "2px solid white";
-    button2.style.color = "white";
-    button2.style.backgroundColor = "black";
-    button2.style.textAlign = "center"
-    button2.classList.add("one-row");
-    lettersOneRow.appendChild(button2);
+
     
     
     
